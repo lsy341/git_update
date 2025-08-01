9b69c4aaf1de4ce5e13651648a3ab2d9b41dd04f
lsy341 <128277691+lsy341@users.noreply.github.com>
2025-08-01 19:11:55 +0900

    1

---
+++ main.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+import pyautogui
+import pyperclip
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+import sys
+from datetime import datetime
+
+# 크롬 드라이버 자동 업데이트
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+
+# 브라우저 꺼짐 방지
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
+
+# 불필요한 에러 메시지 없애기
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
+
+
+service = Service(executable_path=ChromeDriverManager().install())
+
+# pyqt 부분
+import os
+
+# 유효기간 지정
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+UI_PATH = "design.ui"
+
+# 로그인 클래스 생성
+class first(QDialog):
+    def __init__(self):
+        QDialog.__init__(self, None)
+        uic.loadUi(os.path.join(BASE_DIR, UI_PATH), self)
+
+        # 1) 버튼 클릭 이벤트
+        # self.객체이름.clicked.connect(self.실행함수이름)
+        self.close_btn.clicked.connect(self.close)
+
+
+
+    def close(self):
+        sys.exit()
+
+QApplication.setStyle("fusion")
+app = QApplication(sys.argv)
+main_dialog = first()
+main_dialog.show()
+
+sys.exit(app.exec_())
